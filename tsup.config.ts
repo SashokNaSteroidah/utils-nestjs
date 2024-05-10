73889d0087077f8442169ebc7d094d6b200ba690
merch2 <merch2@bwg-io.site>
2024-05-10 20:48:38 +0300

    dev: update github repo

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     minify: "terser",
+    tsconfig: "./tsconfig.json",
     treeshake: "recommended",
     entry: ["src/index.ts"],
     format: ["cjs", "esm"],
@@ -9,4 +10,12 @@ export default defineConfig({
     splitting: false,
     sourcemap: true,
     clean: true,
+    bundle: true,
+    terserOptions: {
+        compress: true,
+        module: true,
+        keep_classnames: false,
+        sourceMap: true,
+        keep_fnames: false
+    }
 });
