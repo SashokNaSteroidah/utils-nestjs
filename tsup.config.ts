6f16edcb7dd14e4dbc9184f055a2c3ae761b4adf
merch2 <merch2@bwg-io.site>
2024-05-15 03:01:33 +0300

    0.0.8-beta.2.2: tsup legacy

---
+++ tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    minify: "terser",
+    tsconfig: "./tsconfig.json",
+    treeshake: "recommended",
+    entry: ["src/index.ts"],
+    format: ["cjs", "esm"],
+    dts: true,
+    splitting: false,
+    legacyOutput: true,
+    sourcemap: true,
+    clean: true,
+    bundle: true,
+    terserOptions: {
+        compress: true,
+        module: true,
+        keep_classnames: false,
+        sourceMap: true,
+        keep_fnames: false
+    }
+});
